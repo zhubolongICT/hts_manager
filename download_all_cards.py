4e6d114dc6b923b73f1ad6c4308b4ae00d2fd5d0
朱伯龙 <chouisbo@163.com>
2025-03-12 10:15:40 +0800

    update download_all_cards

--- download_all_cards.py
+++ download_all_cards.py
@@ -6,16 +6,13 @@ from hts.DeckManager import DeckManager
 if __name__ == "__main__":
     manager = DeckManager(
         cards_json_filepath='data/cards.json',
-        images_cache_dirpath='imagecache')
+        images_cache_dirpath='/mnt/d/imagecache')
 
     dbfId2CardsMap = manager.get_dbfId2CardsMap()
-    
     pbar = tqdm(total=len(dbfId2CardsMap))
     for dbfId, card in dbfId2CardsMap.items():
         manager.get_image_filepath_by_dbfId(dbfId)
         pbar.update(1)
         time.sleep(0.5)
-        break
     pbar.close()
-    
 
